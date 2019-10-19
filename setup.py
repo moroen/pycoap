f7733b3a274576505082c9bb99932734ec3287df
moroen <moroen@gmail.com>
2019-10-19 20:02:40 +0200

    Bump version: 0.3.1 → 0.3.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name="pycoap",
     description="A low level extension for COAP/COAPS-requests",
     url="https://github.com/moroen/python-coap-module",
-    version="0.3.1",
+    version="0.3.2",
     author="moroen",
     author_email="no@email.com",
     classifiers=[
