31795f15a387e5a0bc03c83c6f6199912d4b3dfc
moroen <moroen@users.noreply.github.com>
2019-10-25 22:58:13 +0200

    Manylinux (#7)
    * Create build_wheels.sh
    
    * Create build_manylinux
    
    * Rename build_manylinux to build_manylinux.yml
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Manylinux
    
    * Fixed script name
    
    * Updated azure-pipelines

---
+++ azure/build_wheels.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e -x
+
+# Collect the pythons
+pys=(/opt/python/*/bin)
+
+# Filter out Python2
+pys=(${pys[@]//*2*/})
+
+# Filter out Python 3.4
+pys=(${pys[@]//*34*/})
+
+# Install go
+yum -y install wget
+wget https://dl.google.com/go/go1.13.1.linux-amd64.tar.gz
+tar -xzf go1.13.1.linux-amd64.tar.gz
+
+export PATH=$PATH:`pwd`/go/bin
+
+# Compile wheels
+for PYBIN in "${pys[@]}"; do
+    "${PYBIN}/pip" install -r /io/azure/dev-requirements.txt
+    "${PYBIN}/pip" wheel /io/ -w wheelhouse/
+done
+
+# Bundle external shared libraries into the wheels
+for whl in wheelhouse/$package_name-*.whl; do
+    auditwheel repair --plat $PLAT "$whl" -w /io/wheelhouse/
+done
