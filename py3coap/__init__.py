effb6f1a629d9a745ba25575175a9e798a8b0f7d
moroen <moroen@gmail.com>
2020-12-16 22:36:49 +0100

    Bump version: 0.9.1 → 0.9.2

--- py3coap/__init__.py
+++ py3coap/__init__.py
@@ -1,4 +1,4 @@
 from .py3coap import Request, setDebugLevel, GET, PUT, POST, CloseConnection
 # from _pycoap import UriNotFoundError, HandshakeError, MethodNotAllowedError
 
-__version__ = "0.9.1"
+__version__ = "0.9.2"
